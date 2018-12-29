2fce4c3d7b4d26564cf61d1de17f776448bb013e
Alexander Lundborg <alexander.lundborg@gmail.com>
2018-12-29 01:01:52 +0100

    yadda yadda

---
+++ mailsenderproper.php
@@ -0,0 +1,34 @@
+<?php
+require_once "Mail.php"; //Importera Pear Mail::factory bibloteket
+
+
+$pickup = $_POST['pickup'];
+$dropoff = $_POST['dropoff'];
+$name = $_POST['name'];
+$name = $_POST['phone'];
+
+$from = "testadress@jacobolsson.se"; //Ange avsändare här
+$to = "alexander.lundborg@gmail.com"; //Ange mottagaren här
+$subject = "Ny kundförfrågan (med mb_convert_encoding och Content-Transfer-Encoding: 8bit)"; //Ange titeln på meddelandet här
+$subject = mb_convert_encoding($subject, "UTF-8");
+
+$body = "Hej!\n\n $name önskar att bli upphämtad vid $pickup och avsläppt vid $dropoff."; //Ditt meddelande
+$body = mb_convert_encoding($body, "UTF-8");
+	
+$host = "smtp.fsdata.se"; //SMTP-server
+$port = "26"; //Port för SMTP
+$username = "testadress@jacobolsson.se"; //Ditt användarnamn för mailkontot
+$password = "abc123"; //Ditt lösenord för mailkontot
+
+/* Import av variabler till arrayer som används för att skicka iväg e-post */
+$headers = array ('From' => $from,'To' => $to,'Subject' => $subject, "Content-Type: text/html;charset=utf-8", "Content-Transfer-Encoding: 8bit");
+$smtp = Mail::factory('smtp',array ('host' => $host,'port' => $port,'auth' => true,'username' => $username,'password' => $password));
+$mail = $smtp->send($to, $headers, $body);
+
+/* Visa ett meddelande om mailet har gått iväg eller visa ett tekniskt felmeddelande om det uppstår problem */
+if (PEAR::isError($mail)) {
+echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+    header('Location: thank-you.html');
+}
+?>
